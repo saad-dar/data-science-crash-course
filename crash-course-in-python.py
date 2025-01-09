514cc397876be0572d914fd7261e0614da81df97
saadullah dar <saadullahdar.official@gmail.com>
2025-01-09 17:55:50 +0500

    modules

--- crash-course-in-python.py
+++ crash-course-in-python.py
@@ -25,3 +25,24 @@ for i in [1, 2, 3, 4, 5]:
     # notice the blank line
     print(i)
 
+# modules
+
+import re
+my_regex = re.compile("[0-9]+", re.I)
+
+# importing module with alias
+
+import re as regex
+# import matplotlib.pyplot as plt
+my_regex = regex.compile("[0-9]+", regex.I)
+
+# importing specific functions from a module
+
+from collections import defaultdict, Counter
+loopup = defaultdict(int)
+my_counter = Counter()
+
+# importing all functions from a module into the global namespace (usually discouraged)
+match = 10
+from re import *
+print(match) # "<function re.match>" # above match value is replaced by re.match function
