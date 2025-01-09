427bae3df3587cdab338c86ea5c054ce7329d742
saadullah dar <saadullahdar.official@gmail.com>
2025-01-09 16:59:19 +0500

    white spaces formatting

---
+++ crash-course-in-python.py
@@ -0,0 +1,27 @@
+# white spaces formatting
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+    for j in [1, 2, 3, 4, 5]:
+        print(j)
+        print(i + j)
+    print('again')
+    print(i)
+print("done looping")
+
+# long winding list computation
+long_winded_computation = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 
+                           + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20)
+
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+easy_to_read_list_of_lists = [[1, 2, 3],
+                                [4, 5, 6],
+                                [7, 8, 9]]
+
+two_plus_three = 2 + \
+                    3
+for i in [1, 2, 3, 4, 5]:
+
+    # notice the blank line
+    print(i)
+
