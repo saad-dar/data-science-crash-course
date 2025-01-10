757c5cb3560a8e960fba8f19116cf4ff1dba653a
saadullah dar <saadullahdar.official@gmail.com>
2025-01-10 12:38:55 +0500

    lists in python

--- crash-course-in-python.py
+++ crash-course-in-python.py
@@ -115,3 +115,61 @@ try:
     print(0 / 0)
 except ZeroDivisionError:
     print("cannot divide by zero")
+
+# list
+integer_list = [1, 2, 3]
+heterogeneous_list = ["string", 0.1, True]
+list_of_lists = [integer_list, heterogeneous_list, []]
+
+list_length = len(integer_list) # 3
+print(list_length) # 3
+list_sum = sum(integer_list) # 6
+print(list_sum) # 6
+
+# setting or getting the nth element of a list
+
+x = range(10) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+zero = x[0] # 0
+one = x[1] # 1
+nine = x[-1] # 9
+eight = x[-2] # 8
+print(x)
+x[0] = -1 # [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9] # TypeError: 'range' object does not support item assignment in python 3
+print(x) # [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# slicing
+first_three = x[:3] # [-1, 1, 2]
+three_to_end = x[3:] # [3, 4, 5, 6, 7, 8, 9]
+one_to_four = x[1:5] # [1, 2, 3, 4]
+last_three = x[-3:] # [7, 8, 9]
+without_first_and_last = x[1:-1] # [1, 2, 3, 4, 5, 6, 7, 8]
+copy_of_x = x[:] # [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# check if a value is in a list
+print(1 in [1, 2, 3]) # True
+print(0 in [1, 2, 3]) # False
+
+# concatenating lists
+x = [1, 2, 3]
+x.extend([4, 5, 6]) # x is now [1, 2, 3, 4, 5, 6] # modifies the list in place
+
+# if you don't want to modify the original list you can use list addition
+x = [1, 2, 3]
+y = x + [4, 5, 6] # y is [1, 2, 3, 4, 5, 6] # creates a new list
+
+# append method adds a single element to the end of the list
+
+x = [1, 2, 3]
+x.append(0) # x is [1, 2, 3, 0]
+y = x[-1] # 0
+z = len(x) # 4
+print(z, y, x)
+
+# unpacking lists (similar to tuple unpacking)
+x, y = [1, 2] # x is 1, y is 2
+
+# pythonic way to swap variables
+x, y = y, x # x is 2, y is 1
+
+# underscores can be used to assign values to unused variables
+_, y = [1, 2] # y is 2 and 1 will be ignored
